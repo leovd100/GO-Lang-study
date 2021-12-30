29deeaef6ff1e11960355005db1fe663df30f66e
Leonardo <demetrio.vd@hotmail.com>
2021-12-29 21:20:42 -0300

    logical operators

---
+++ fundamentos/logicos/logicos.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func compras(trab1, trab2 bool) (bool, bool, bool) {
+	comprarTV50 := trab1 && trab2
+	comprarTV32 := trab1 != trab2 // ou exclusivo
+	comprarSorvete := trab1 || trab2
+
+	return comprarTV50, comprarTV32, comprarSorvete
+}
+
+func main() {
+	tv50, tv32, sorvete := compras(true, true)
+	fmt.Printf("Tv50: %t, Tv32: %t, Saudável: %t", tv50, tv32, !sorvete)
+}
